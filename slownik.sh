635a5418b315d04549157e28ed1d5f2e0fa2c01b
Natalia Kospat Pawłowska <natalia@natalia-VirtualBox.(none)>
2013-01-31 13:15:26 +0100

    dadaje glowny plik

---
+++ slownik.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+#menu
+function menu {
+	echo
+	echo Co chcesz zrobic?
+	echo 1 Znalezc haslo
+	echo 2 Dodac wpis
+	echo 3 Wyjsc
+	echo
+	echo Podaj liczbe:
+	read corobic
+
+	while [ $corobic != 1 ] && [ $corobic != 2 ] && [ $corobic != 3 ]
+	do
+		echo
+		echo Nie rozumiem.
+		echo
+		echo Co chcesz zrobic? 
+		echo 1 Znalezc haslo
+		echo 2 Dodac wpis
+		echo 3 Wyjsc
+		echo
+		echo Podaj liczbe:
+		read corobic
+	done;
+
+	if [ $corobic = 1 ]
+	then
+		ZnalezcHaslo
+	fi
+
+	if [ $corobic = 2 ]
+	then
+		echo Jakie haslo chcesz dodac?
+		read haslo
+		CzyIstnieje $haslo
+	fi
+}
+
+#czy istnieje
+function CzyIstnieje {
+	echo $1 - >spr.txt
+	cat slownik.txt | grep -i -f spr.txt > spr2.txt #sprawdza wzorzec z pliku
+	wc -m spr2.txt | cut -d ' ' -f 1 > wynik.txt #sprawdza ile znakow
+	wynik=`cat "wynik.txt"`
+	
+	if [ $wynik != 0 ] #co gdy haslo juz istnieje
+	then
+		echo Takie haslo juz istnieje.
+		echo "Czy chcesz dopisac kolejny opis do tego hasla?"
+		echo
+		echo 1 Tak
+		echo 2 Nie
+		echo
+		echo Podaj liczbe:
+		read codalej
+							#co robic dalej - pisac czy nie 
+		while [ $codalej != 1 ] && [ $cidalej != 2 ] #TU COS NIE GRA!!!
+		do
+			echo 
+			echo Nie rozumiem.
+			echo
+			echo "Czy chcesz dopisac kolejny opis do tego hasla?"
+			echo
+			echo 1 Tak
+			echo 2 Nie
+			echo
+			echo Podaj liczbe:
+			read codalej
+		done
+
+		if [ $wynik = 1 ]
+		then 
+			DodacWpis $1
+		fi
+
+		if [ $wynik = 2 ]
+		then
+			menu
+		fi
+
+	fi
+
+	rm spr.txt #usuwanie smieci
+	rm spr2.txt
+	rm wynik.txt
+
+	DodacWpis $1
+}
+
+#znalezc haslo
+function ZnalezcHaslo {
+	echo 
+	echo Jakiego hasla szukasz?
+	read haslo
+	echo
+
+	echo $haslo - >spr.txt
+	cat slownik.txt | grep -i -f spr.txt
+
+	echo
+	echo Wcisnij Enter
+	read
+
+	rm spr.txt #usuwanie smieci
+
+	menu
+}
+
+#dadac wpis
+function DodacWpis {
+
+	echo
+	echo Podaj opis hasla
+	read opis
+
+	echo $1 - $opis >> slownik.txt
+	sort slownik.txt > slownik1.txt
+	rm slownik.txt
+	mv slownik1.txt slownik.txt
+
+	menu
+}
+
+menu
+
+
